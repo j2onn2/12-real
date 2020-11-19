0784886f77942ba983cce67e39e1687ea97d056d
j2onn2 <70683011+j2onn2@users.noreply.github.com>
2020-11-19 15:46:06 +0900

    .

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+
+int main (int argc, char *argv []){
+	char src[] = "Thr worst things to eat before you sleep"	;
+	char dst[100]	;
 	
-	int i = 0 ;
-	char str[4]	;
+	char str[30] = "happy C programming"	;
 	
-	str [0] = 'a'	;
-	str[1] = 'b'	;
-	str [2] = 'c'	;
-	str [3] = '\0'	;
+	strcpy(dst, src)	;
 	
-	printf ("%s\n", str)	;
+	printf("copied string : %s\n", dst)	;
 	
-	while ( str [i] != '\0'){ 
-		printf("%c", str[i])	;
-		i++	;
-	}
 	
+	pirntf("문자열\"%s\"의 길이 : %i\n", str, strlen(str))	;
+	
+	return 0	;
 }
+
+
